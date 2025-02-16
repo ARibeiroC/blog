fdc5c5833f2ff0d9c392fb290560d3ce51316484
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2025-02-16 20:46:02 -0300

    fix: app align items

--- src/css/AppCSS.jsx
+++ src/css/AppCSS.jsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     max-width: 1000px;
-    // margin: 2rem auto;
+    margin: 2rem auto;
     padding: 1rem 1rem;
 
     @media (max-width: 768px){
         padding: 0 !important;
+        margin: 0;
     }
 `
