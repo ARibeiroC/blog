0e703bbb6cd3dba929139b4094135cc0cd2237ae
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2025-02-19 20:30:02 -0300

    add: Post responsibity design

--- src/css/PostCSS.jsx
+++ src/css/PostCSS.jsx
@@ -34,4 +34,36 @@ export const PostCSS = styled.div`
             cursor: pointer;
         }
     }
+
+    @media (max-width: 425px){
+        padding: 1rem;
+
+        h2 {
+            text-align: start;
+        }
+
+        .content {
+            border: 1px solid white;
+            border-radius: .6rem;
+            padding: .4rem;
+            box-shadow: 2px 2px 6px -2px var(--color-text);
+        }
+    }
+    @media (max-width: 768px){
+        justify-content: center;
+        gap: 1rem;
+
+        padding: 1rem;
+        
+        h2 {
+            text-align: start;
+        }
+
+        .content {
+            border: 1px solid white;
+            border-radius: .6rem;
+            padding: .4rem;
+            box-shadow: 2px 2px 6px -2px var(--color-text);
+        }
+    }
 `
